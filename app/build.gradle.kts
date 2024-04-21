4dd74348bb87825661ab03941257b90e09f105d9
Zachary Wander <zachary.wander@gmail.com>
2024-04-21 18:09:24 -0400

    Version 1.3.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "tk.zwander.wifilist"
         minSdk = 30
         targetSdk = 34
-        versionCode = 12
-        versionName = "1.3.1"
+        versionCode = 13
+        versionName = "1.3.2"
 
         vectorDrawables {
             useSupportLibrary = true
